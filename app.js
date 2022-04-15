2bf8cf35ee30083c0dd30bf94524f09bd280a32e
FranciscoZanetti <franciscozanetti123@gmail.com>
2022-04-15 16:44:36 -0300

    agregue en app.js el acceso a descripcion de producto

--- app.js
+++ app.js
@@ -18,3 +18,6 @@ app.get('/', (req, res) => {
     app.get('/register', (req, res) => {
         res.sendFile(__dirname + '/views/register.html')
         });
+    app.get('/product', (req, res) => {
+      res.sendFile(__dirname + '/views/descripcionProducto.html')
+      });
