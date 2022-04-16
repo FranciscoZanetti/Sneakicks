e259e89a3385e2db858aa9d61fccfcdbb2d3aa69
FranciscoZanetti <franciscozanetti123@gmail.com>
2022-04-15 22:23:14 -0300

    primer commit de descripcionProducto y archivos complementarios

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + 'public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
