8dc00f23cd7e3b19f8890d5d4cf111a49a92ba87
IthilNah <alankasis@hotmail.com>
2022-05-05 15:38:12 -0300

    Partials

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,21 +1,67 @@
+const productos = [
+    {
+        id: 1,
+        marca: 'Nike',
+        modelo: 'Dunk Low',
+        precio: '$24.000',
+        image_url: 'nike_1.webp'
+    },
+    {
+        id: 2,
+        marca: 'adidas Yeezy',
+        modelo: 'Yeezy Boost 350 V2',
+        precio: '$120.000',
+        image_url: 'yeezy_1.webp'
+    },
+    {
+        id: 3,
+        marca: 'Jordan',
+        modelo: 'Air Jordan 1 High 85',
+        precio: '$140.000',
+        image_url: 'airjordan_1.jpg'
+    },
+    {
+        id: 4,
+        marca: 'Nike',
+        modelo: 'Dunk Low',
+        precio: '$24.000',
+        image_url: 'nike_1.webp'
+    },
+    {
+        id: 5,
+        marca: 'adidas Yeezy',
+        modelo: 'Yeezy Boost 350 V2',
+        precio: '$120.000',
+        image_url: 'yeezy_1.webp'
+    },
+    {
+        id: 6,
+        marca: 'Jordan',
+        modelo: 'Air Jordan 1 High 85',
+        precio: '$140.000',
+        image_url: 'airjordan_1.jpg'
+    },
+
+]
+
 const controller = {
     index: (req, res) => {
-        return res.render('index');
+        return res.render('products/index', {productos: productos});
     },
     login: (req, res) => {
-        return res.render('login');
+        return res.render('users/login');
     },
     register: (req, res) => {
-        return res.render('register');
+        return res.render('users/register');
     },
     productDetail: (req, res) => {
-        return res.render('productDetail');
+        return res.render('products/productDetail');
     },
     cart: (req, res) => {
-        return res.render('cart');
+        return res.render('products/cart');
     },
     manageProduct: (req, res) => {
-        return res.render('manageProduct');
+        return res.render('products/manageProduct');
     },
 }
 
