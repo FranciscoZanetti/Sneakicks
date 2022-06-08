2ea0e898812ad1d56d9d152f939313a82bc4aa54
IthilNah <alankasis@hotmail.com>
2022-06-08 11:01:40 -0300

    commit para pull

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000
 const mainRoutes = require('./routes/mainRoutes');
 const productRoutes = require('./routes/productRoutes');
 const methodOverride = require('method-override');
+const session = require('express-session');
 
 app.listen(port, () => {
   console.log(`Servidor iniciado en puerto ${port} - http://localhost:${port}`)
@@ -20,3 +21,15 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(methodOverride('_method'));
+
+app.use(session( { secret: "Aca va el secret" }))
+
+router.get('/pruebaSession', function(req, res) {
+  if (req.session.numeroDeVisitas == undefined) {
+      req.session.numeroDeVisitas = 0;
+  }
+
+  req.session.numeroDeVisitas++;
+
+  res.send("Visita numero:" + req.session.NumeroDeVisitas)
+})
